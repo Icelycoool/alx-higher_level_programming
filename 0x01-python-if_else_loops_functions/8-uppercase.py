4e8fbf94dd7aa0c4f5ca15177daf5ea586a751e3
Icelycoool <mohammedcali@proton.me>
2023-11-28 13:51:01 -0800

    A unction that prints a string in uppercase followed by a new line.

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            upper_char = chr(ord(char) - 32)
+            print(upper_char, end="")
+        else:
+            print(char, end="")
+    print()
