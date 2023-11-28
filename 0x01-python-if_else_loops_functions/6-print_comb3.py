063d73d2fb8e33c08b093ce02f702c1a2689c5c3
Icelycoool <mohammedcali@proton.me>
2023-11-28 12:58:55 -0800

    fixed style requirement

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 for i in range(10):
-    for j in range(i + 1,10):
+    for j in range(i + 1, 10):
         if i != j:
             if i == 0 and j == 1:
                 print("01", end=", " if i != 8 or j != 9 else "\n")
             else:
-                print("{:d}{:d}".format(i, j), end=", " if i != 8 or j != 9 else "\n")
+                print("{:d}{:d}".format(i, j),
+                      end=", " if i * 10 + j < 89 else "\n")
