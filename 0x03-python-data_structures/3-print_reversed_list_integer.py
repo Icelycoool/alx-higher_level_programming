f66318f2340ad89506e9650e8b331a69a7cdd59e
Icelycoool <mohammedcali@proton.me>
2024-05-27 16:50:57 -0700

    Correcting output error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    reversed_list = my_list[::-1]
-    for number in reversed_list:
-        print("{:d}".format(number))
+    if my_list:
+        for number in reversed(my_list):
+            print("{:d}".format(number))
