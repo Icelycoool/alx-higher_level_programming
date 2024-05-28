543969e27b07cdf1f336d4e41d65e7fbcc82d0c9
Icelycoool <mohammedcali@proton.me>
2024-05-28 05:24:17 -0700

    Correct requirement for task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0] if length > 0 else "None"
-    new = length, first
+    total = len(sentence)
+    char = sentence[0] if length > 0 else "None"
+    new = total, char
     return(new)
