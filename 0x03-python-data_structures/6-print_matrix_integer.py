3f44690276228f2395473eb0b3fb26daca042d37
Icelycoool <mohammedcali@proton.me>
2024-05-28 03:30:28 -0700

    Correcting output

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
-        print(''.join("{:d}".format(number) for number in list))
+        print(' '.join("{:d}".format(number) for number in list))
