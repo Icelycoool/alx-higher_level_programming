66a44dead63b876df35231acae33598bb5b2a3ab
Icelycoool <mohammedcali@proton.me>
2024-05-28 23:43:05 -0700

    Task 9 requirement correction

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    multiplied_dict = {}
-    for key, value in a_dictionary.items():
-        multiplied_dict[key] = value * 2
+    multiplied_dict = a_dictionary.copy()
+    for key in multiplied_dict:
+        multiplied_dict[key] *= 2
     return multiplied_dict
 
