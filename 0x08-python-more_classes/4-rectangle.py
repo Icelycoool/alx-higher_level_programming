56fba1b66579aceaf6b2f1f719bae63d5c419e26
Icelycoool <mohammedcali@proton.me>
2024-06-10 13:51:16 -0700

    Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -93,6 +93,7 @@ class Rectangle:
         for _ in range(self.height):
             rectangle_str += "#" * self.width + "\n"
         return rectangle_str.strip()
+
     def __repr__(self):
         """
         Returns a string representation of the rectangle to recreate
