b95f96d48c5f208155969721d1367215eb424244
Icelycoool <mohammedcali@proton.me>
2024-06-12 05:24:53 -0700

    11. Square #2

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,8 +9,8 @@ class Square(Rectangle):
     """Inherits from Rectangle"""
 
     def __init__(self, size):
-        if self.integer_validator('size', size):
-            self.__size = size
+        self.integer_validator('size', size)
+        self.__size = size
         super().__init__(size, size)
 
     def area(self):
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,8 +9,8 @@ class Square(Rectangle):
     """Inherits from Rectangle"""
 
     def __init__(self, size):
-        if self.integer_validator('size', size):
-            self.__size = size
+        self.integer_validator('size', size)
+        self.__size = size
         super().__init__(size, size)
 
     def area(self):
@@ -18,4 +18,4 @@ class Square(Rectangle):
         return super().area()
 
     def __str__(self):
-        return super().__str__()
+        return "[Square] {}/{}".format(self.__size, self.__size)
