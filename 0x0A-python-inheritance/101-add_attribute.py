e3e4ed971c02d2e4e8152e3988a7e7353e902b6f
Icelycoool <mohammedcali@proton.me>
2024-06-12 05:47:01 -0700

    13. Can I?

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module add_attribute"""
+
+
+def add_attribute(obj, attr, val):
+    """"
+    Adds a new attribute to the given object.
+
+    Parameters:
+        obj: The object to which the attribute will be added.
+        attr: The name of the attribute.
+        val: The value of the attribute.
+
+    Raises:
+        TypeError:
+            If the object does not support adding new attributes.
+    """
+
+    if not hasattr(obj, '__dict__') and not hasattr(obj, '__slots__'):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attr, val)
