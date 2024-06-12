fb9b3ffaddece9d2d46cd88cca23bfc5b191daa2
Icelycoool <mohammedcali@proton.me>
2024-06-12 01:29:58 -0700

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module BaseGeometry"""
+
+
+class BaseGeometry:
+    """Empty Class"""
+    pass
+
+    def area(self):
+        """Method that should be implemented by subclasses"""
+        raise Exception("area() is not implemented")
