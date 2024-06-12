b73b3be322dc6f0ea25d5a079d34700fcde0971b
Icelycoool <mohammedcali@proton.me>
2024-06-12 05:10:26 -0700

    10. Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Inherits from Rectangle"""
+
+    def __init__(self, size):
+        if self.integer_validator('size', size):
+            self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Calculate the area of the square"""
+        return super().area()
