556aa114ab1e58c10a4928881ee823bb19a8c677
Icelycoool <mohammedcali@proton.me>
2024-06-12 04:11:22 -0700

    8. Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module BaseGeometry"""
+
+
+class BaseGeometry:
+    """Empty Class"""
+    pass
+
+    def area(self):
+        """Method that should be implemented by subclasses"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates that value is a positive integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The value to validate.
+
+        Returns:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    Inherits from BaseGeometry.
+
+    Methods:
+        __init__(self, width, height): Initialized the rectangle clas.
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
