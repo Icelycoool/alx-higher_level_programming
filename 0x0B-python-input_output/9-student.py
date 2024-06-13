4db2fa02473a6b123d879421a3ec709c85ab868d
Icelycoool <mohammedcali@proton.me>
2024-06-13 13:44:46 -0700

    9. Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module that defines a student class
+"""
+
+
+class Student():
+    """
+    Student class
+
+    Methods:
+        __init__(self, first_name, last_name, age):
+            Inializes the student class.
+        to_json(self):
+            Retrieves a dictionary representation of a student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initializes the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a student."""
+        return self.__dict__
