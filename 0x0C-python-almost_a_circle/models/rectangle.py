0e5d3aab1ee546e7009e76e38b5efa47820e5438
Icelycoool <mohammedcali@proton.me>
2024-06-15 01:27:35 +0300

    6. __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,3 +88,8 @@ class Rectangle(Base):
         """Print the rectangle using the `#` character"""
         for _ in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        """Returns a string representation of the rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
