7e956e8862a9ebb08c255fafbc78b7b240df82ab
Icelycoool <mohammedcali@proton.me>
2024-06-15 14:06:18 +0300

    13. Rectangle instance to dictionary representation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -113,6 +113,16 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
+
     def __str__(self):
         """Returns a string representation of the rectangle"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
