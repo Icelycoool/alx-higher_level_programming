2b71e719f33e818c2b6c8bec8856f230033e6298
Icelycoool <mohammedcali@proton.me>
2024-06-23 10:54:00 -0700

    Requirements correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
+"""
+A script that lists all states from the database hbtn_0e_0_usa
+"""
+
+
 import MySQLdb
 from sys import argv
-"""A script that lists all states from the database hbtn_0e_0_usa"""
 
 
 if __name__ == "__main__":
 
     # Connecting tot he database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
 
-    for i in rows:
+    row = cur.fetchall()
+
+    for i in row:
         print(i)
 
     cur.close()
