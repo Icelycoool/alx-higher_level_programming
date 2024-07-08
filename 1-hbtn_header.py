8440fa672472ed8f9bc826373a04961d7426895d
Icelycoool <mohammedcali@proton.me>
2024-07-08 00:48:08 -0700

    1. Response header value #0

--- 1-hbtn_header.py
+++ 1-hbtn_header.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-# A script that takes in a URL, sends a request to the URL and displays the
-# value of the X-Request-Id variable found in the header of the response.
+""" A script that takes in a URL, sends a request to the URL and displays the
+value of the X-Request-Id variable found in the header of the response."""
 import sys
 import urllib.request
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    headers = response.getheaders()
-    x_request_id = dict(headers).get("X-Request-Id")
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        headers = response.getheaders()
+        x_request_id = dict(headers).get("X-Request-Id")
 
 print(x_request_id)
