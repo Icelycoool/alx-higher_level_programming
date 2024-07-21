629020780d71395c0c6347f7d2713ab99ea5c92d
Icelycoool <mohammedcali@proton.me>
2024-07-21 08:02:13 -0700

    From task two to task five

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- Creates a table first_table with the following columns.
+--	id INT
+-- 	name VARCHAR(256)
+-- The database name will be passed as an argument of the mysql command.
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+);
