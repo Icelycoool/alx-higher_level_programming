216a87fbfcaec75a73d962bfb0591939be3df3d9
Icelycoool <mohammedcali@proton.me>
2024-07-24 15:20:15 -0700

    15. Only Comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,7 +3,7 @@ SELECT t.`title` AS title
 FROM `tv_shows` AS t
 INNER JOIN `tv_show_genres` AS tg
 ON t.`id` = tg.`show_id`
-INNER JOIN `tv_genres` as g
+INNER JOIN `tv_genres` AS g
 ON g.`id` = tg.`genre_id`
 WHERE g.`name` = 'Comedy'
 ORDER BY title;
