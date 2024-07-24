73d85b0e1b49bd313c9962bbc675f47af509f9ec
Icelycoool <mohammedcali@proton.me>
2024-07-24 15:18:59 -0700

    15. Only Comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- Lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT t.`title` AS title
+FROM `tv_shows` AS t
+INNER JOIN `tv_show_genres` AS tg
+ON t.`id` = tg.`show_id`
+INNER JOIN `tv_genres` as g
+ON g.`id` = tg.`genre_id`
+WHERE g.`name` = 'Comedy'
+ORDER BY title;
