19e430f9d39764b933a9ef596eb89e2294b14947
Icelycoool <mohammedcali@proton.me>
2024-07-24 22:11:31 +0300

    6. States table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa).
+CREATE TABLE IF NOT EXISTS states (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(256)
+);
